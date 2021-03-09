feee905f01bc4427855c152fb25402aa1e3859a6
ClassDemo <108B09105@mailst.cjcu.edu.tw>
2021-03-09 11:06:02 +0800

    start app myapp

---
+++ myapp/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.contrib import admin
+from .models import Post, NewTable, Product
+
+# Register your models here.
+
+admin.site.register(Post)
+admin.site.register(NewTable)
+admin.site.register(Product)
+# Register your models here.
